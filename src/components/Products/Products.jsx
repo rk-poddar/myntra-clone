d2609a20654ec15f164b9baf9249744cae6d96df
Rahul Kumar <contactat.rahulkumar@gmail.com>
2023-03-27 12:28:47 +0530

    add search filter and bug fix

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -10,7 +10,7 @@ const Products = () => {
     useEffect(() => {
       dispatch(fetchProducts())
       localStorage.setItem('items', JSON.stringify(products));
-  },[products])
+  },[])
 
   return (
     <>
