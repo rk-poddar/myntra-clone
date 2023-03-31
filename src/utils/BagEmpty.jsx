51dda9d24f852601573323611b56da63b0e7b385
Rahul Kumar <contactat.rahulkumar@gmail.com>
2023-03-31 16:22:51 +0530

    minor changes

--- src/utils/BagEmpty.jsx
+++ src/utils/BagEmpty.jsx
@@ -14,23 +14,23 @@ const BagEmpty = () => {
                 <button>ADD ITEMS FROM WISHLIST</button>
             </Link>
         </div>
-        <div class="checkout-footer"> 
-            <div class="content">
-                <div class="images"> 
-                    <img class="footer-bank-ssl" src="https://constant.myntassets.com/checkout/assets/img/footer-bank-ssl.png" width="70" height="37" /> 
-                    <img class="footer-bank-visa" src="https://constant.myntassets.com/checkout/assets/img/footer-bank-visa.png" width="60" height="37" /> 
-                    <img class="footer-bank-mc" src="https://constant.myntassets.com/checkout/assets/img/footer-bank-mc.png" width="60" height="37" /> 
-                    <img class="footer-bank-ae" src="https://constant.myntassets.com/checkout/assets/img/footer-bank-ae.png" width="60" height="37" /> 
-                    <img class="footer-bank-dc" src="https://constant.myntassets.com/checkout/assets/img/footer-bank-dc.png" width="60" height="37" /> 
-                    <img class="footer-bank-nb" src="https://constant.myntassets.com/checkout/assets/img/footer-bank-nb.png" width="60" height="37" /> 
-                    <img class="footer-bank-cod" src="https://constant.myntassets.com/checkout/assets/img/footer-bank-cod.png" width="60" height="37" /> 
-                    <img class="footer-bank-rupay" src="https://constant.myntassets.com/checkout/assets/img/footer-bank-rupay.png" width="60" height="37" /> 
-                    <img class="footer-bank-paypal" src="https://constant.myntassets.com/checkout/assets/img/footer-bank-paypal.png" width="60" height="37" /> 
-                    <img class="footer-bank-bhim" src="https://constant.myntassets.com/checkout/assets/img/footer-bank-bhim.png" width="60" height="37" /> 
+        <div className="checkout-footer"> 
+            <div className="content">
+                <div className="images"> 
+                    <img className="footer-bank-ssl" src="https://constant.myntassets.com/checkout/assets/img/footer-bank-ssl.png" width="70" height="37" /> 
+                    <img className="footer-bank-visa" src="https://constant.myntassets.com/checkout/assets/img/footer-bank-visa.png" width="60" height="37" /> 
+                    <img className="footer-bank-mc" src="https://constant.myntassets.com/checkout/assets/img/footer-bank-mc.png" width="60" height="37" /> 
+                    <img className="footer-bank-ae" src="https://constant.myntassets.com/checkout/assets/img/footer-bank-ae.png" width="60" height="37" /> 
+                    <img className="footer-bank-dc" src="https://constant.myntassets.com/checkout/assets/img/footer-bank-dc.png" width="60" height="37" /> 
+                    <img className="footer-bank-nb" src="https://constant.myntassets.com/checkout/assets/img/footer-bank-nb.png" width="60" height="37" /> 
+                    <img className="footer-bank-cod" src="https://constant.myntassets.com/checkout/assets/img/footer-bank-cod.png" width="60" height="37" /> 
+                    <img className="footer-bank-rupay" src="https://constant.myntassets.com/checkout/assets/img/footer-bank-rupay.png" width="60" height="37" /> 
+                    <img className="footer-bank-paypal" src="https://constant.myntassets.com/checkout/assets/img/footer-bank-paypal.png" width="60" height="37" /> 
+                    <img className="footer-bank-bhim" src="https://constant.myntassets.com/checkout/assets/img/footer-bank-bhim.png" width="60" height="37" /> 
                     </div> 
-                    <a href="/contactus" class="contact-us"> 
+                    <Link to="/contactus" className="contact-us"> 
                         <span>Need Help ? Contact Us</span> 
-                    </a> 
+                    </Link> 
                 </div>
             </div>
         </div>
--- src/utils/WishlistEmpty.jsx
+++ src/utils/WishlistEmpty.jsx
@@ -11,20 +11,20 @@ export const WishlistEmpty = () => {
         <div data-reactroot="">
           <div>
             <div>
-              <div class="wishlistEmpty-container">
-                <div class="wishlistEmpty-heading">YOUR WISHLIST IS EMPTY</div>
-                <div class="wishlistEmpty-info">Add items that you like to your wishlist. Review them anytime and easily move them to the bag.</div>
-                <div class="myntraweb-sprite wishlistEmpty-icon sprites-emptyIcon">
+              <div className="wishlistEmpty-container">
+                <div className="wishlistEmpty-heading">YOUR WISHLIST IS EMPTY</div>
+                <div className="wishlistEmpty-info">Add items that you like to your wishlist. Review them anytime and easily move them to the bag.</div>
+                <div className="myntraweb-sprite wishlistEmpty-icon sprites-emptyIcon">
                   <img src="https://img.icons8.com/color/256/shopaholic.png" alt="img" />
                 </div>
                   <div>
-                    <Link to="/" class="wishlistEmpty-button">CONTINUE SHOPPING</Link>
+                    <Link to="/" className="wishlistEmpty-button">CONTINUE SHOPPING</Link>
                   </div>
                 </div>
               </div>
             </div>
           <div>
-            <div class="application-base"></div>
+            <div className="application-base"></div>
           </div>
         </div>
       </div>
